6e6c3be0bf46b1af64b777edd9d32a957490984a
Ethan <ethan808080@gmail.com>
2026-03-17 08:15:59 -0700

    Hopefully this fixes it!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 import { defineConfig } from "astro/config";
 
-import preact from "@astrojs/preact";
-
 export default defineConfig({
   site: "https://ethangrayengineer.netlify.app/",
-  integrations: [preact()]
 });
